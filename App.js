7c5ecb8ae41e89c54485d5d64ad7653f64256a19
FxIvan <almendraivan210814@gmail.com>
2023-08-01 21:12:46 -0300

    cambiando ruta de la carpeta login

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./src/components/login";
+import LoginScreen from "./src/screens/login";
+
 
 export default function App() {
   const Stack = createNativeStackNavigator();
